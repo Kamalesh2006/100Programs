dbe2ab65c3d0a27280bb3133dab7bc804201d605
Kamalesh K <79313198+Kamalesh2006@users.noreply.github.com>
2023-01-21 14:39:09 +0530

    Create ValidParanthesis.java

---
+++ ValidParanthesis.java
@@ -0,0 +1,33 @@
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()==0){
+            return true;
+        }
+        Stack<Character> stack = new Stack<>();
+        char[] ch = s.toCharArray();
+
+        for(int i =0;i<s.length();i++){
+
+            if(ch[i]=='{' || ch[i]=='('|| ch[i]=='['){
+                stack.push(ch[i]);
+            }
+            
+            else if(ch[i]=='}'|| ch[i]==']'||ch[i]==')'){
+                if(stack.isEmpty()){
+                    return false;
+                }
+
+                else if((ch[i]=='}' && stack.peek()=='{')||
+                    (ch[i]==')' && stack.peek()=='(')||
+                    (ch[i]==']' && stack.peek()=='[')){
+                        stack.pop();
+                }
+                else{
+                    stack.push(ch[i]);
+                }
+            }
+        }
+       
+        return stack.isEmpty();
+    }
+}
